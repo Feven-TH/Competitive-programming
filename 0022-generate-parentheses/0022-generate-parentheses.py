010814bc2f9dc86a62d3c59d95bc97265163efb9
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-03-24 22:29:45 +0300

    Time: 3 ms (30.42%), Space: 18.1 MB (44.06%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(curr , opens , close):
+            if close > opens:
+                return
+            if len(curr) == 2*n:
+                if opens == close:
+                    res.append(''.join(curr))
+                return
+            
+            for bracket in '()':
+                curr.append(bracket)
+                if bracket == '(':
+                    opens +=  1
+                else:
+                    close += 1
+            
+                backtrack(curr, opens , close)
+                curr.pop()
+        
+                if bracket == '(':
+                    opens -=  1
+                else:
+                    close -= 1
+
+
+        backtrack([], 0 , 0)
+        return res
+
+# test = Solution()
+# print("here", test.generateParenthesis(8))
