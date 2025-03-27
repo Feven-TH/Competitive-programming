1d7450d2e5265b7d886a4d6fb2f010c7eeaa89c5
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-03-27 19:50:50 +0300

    Time: 3 ms (60.85%), Space: 18 MB (28.87%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = [] 
+        path = []
+
+        def bt(start):
+            # base
+            if len(path) == 4:
+                if start == len(s):
+                    ans.append(".".join(path))
+                return
+
+
+
+            for end in range(start, len(s)):
+                cur_slice = s[start:end+1]
+                if len(cur_slice) > 1 and cur_slice[0] == "0":
+                    break
+                
+                if int(cur_slice) > 255:
+                    break
+                
+                path.append(cur_slice)
+                bt(end + 1)
+                path.pop()
+ 
+        if not 4 <= len(s) <= 12:
+            return []
+
+        bt(0)
+        return ans
+        
