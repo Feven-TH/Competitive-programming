00ceeaca47dcdac8e0c03490653d4d726f3aca12
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-03-28 22:57:03 +0300

    Time: 1104 ms (5.11%), Space: 30.6 MB (25.24%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        def validate(mid):
+            prev = position[0]
+            count = 1
+            for i in range(1 , len(position)):
+                curr = position[i]
+                if curr - prev >= mid:
+                    prev = curr
+                    count += 1
+                if count == m:
+                    return True
+        
+        position.sort()
+        low = 1
+        high = 2**40
+        res = low
+        while low <= high:
+            mid = (low + high)//2
+            if validate(mid):
+                res = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return res
