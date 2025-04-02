c76e289d8cf1872e9b5904415f2039d74d594cbb
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-04-02 18:17:08 +0300

    Time: 13 ms (21.81%), Space: 19.2 MB (6.58%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for num in nums:
+            bucket[num].append(num)
+        
+        for i in range(len(bucket)):
+            if len(bucket[i]) == 0:
+                return i 
+        
