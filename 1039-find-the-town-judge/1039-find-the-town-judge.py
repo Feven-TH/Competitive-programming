35572f5bb4f04e4d512fc80991c80953b8086280
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-04-07 22:39:26 +0300

    Time: 17 ms (48.48%), Space: 21.1 MB (7.82%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        maps = defaultdict(set)
+        for i,j in trust:
+            maps[i].add(j)
+        trusted = []
+        for val in maps.values():
+            for v in val:
+                trusted.append(v)
+        # print("maps:" , maps)
+        # print("trusted:" , trusted)
+        for i in range(1, n+1):
+            if i not in maps and trusted.count(i) == n - 1:
+                return i
+        return -1
