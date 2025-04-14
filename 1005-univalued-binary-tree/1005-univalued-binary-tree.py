1571629cf0056a69f451541f484f74175c6c2815
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-04-14 20:00:25 +0300

    Time: 0 ms (100%), Space: 18 MB (16.77%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.py
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -6,23 +6,18 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        visited = set()
         queue = deque([root])
         val = root.val
     
         while queue:
-
-            for curr in range(len(queue)):
-                if root.val not in visited:
-                    visited.add(root.val)
-                    queue.append(root)
-                curr = queue.popleft()
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-                if curr.val != val:
-                    return False
+            curr = queue.popleft()
+            if curr.val != val:
+                return False
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+            
         return True 
 
             
