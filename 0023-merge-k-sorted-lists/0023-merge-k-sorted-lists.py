99e0d689483b00e1b481f3a89efabf6e7c808118
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-04-29 22:09:19 +0300

    Time: 16 ms (32.74%), Space: 21.7 MB (5.38%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        arr = [[] for i in range(len(lists))]
+        for i in range(len(lists)):
+            curr = lists[i]
+            while curr:
+                arr[i].append(curr.val)
+                curr = curr.next
+        
+        heap = []
+        for i in range(len(arr)):
+            if arr[i]:
+                heapq.heappush(heap, (arr[i][0] ,i ,0))
+        
+        dummy = ListNode(0)
+        head = dummy
+        while heap:
+            val, i ,j =  heappop(heap)
+            dummy.next = ListNode(val)
+            dummy = dummy.next
+            if j + 1 < len(arr[i]):
+                heappush(heap ,(arr[i][j +1] , i, j+ 1)) 
+       
+        return head.next
+            
+
+        
+         
+# change the linked lists to an array
+# for each list in the array append the first element in the heap afterwards append them with index of the list they came with and the index too 
+# when popping push the next element 
+# usie th linked list
