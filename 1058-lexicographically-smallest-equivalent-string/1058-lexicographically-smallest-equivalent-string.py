d91ac2e7cc379fb93e7dad8c6c32bb388b23f843
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-05-10 23:21:41 +0300

    Time: 9 ms (18.5%), Space: 18 MB (23.51%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -1,8 +1,6 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         parent = {chr(i): chr(i) for i in range(97, 124)}
-        minn = {chr(i): chr(i) for i in range(97, 124)}
-
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
@@ -12,10 +10,9 @@ class Solution:
             rootX = find(x)
             rootY = find(y)
             if rootX != rootY:
-                smallest = min(rootX, rootY)  
-                parent[rootX] = parent[rootY] = smallest
-                minn[rootX] = minn[rootY] = smallest
+                minn = min(rootX, rootY)  
+                parent[rootX] = parent[rootY] = minn
 
         for a, b in zip(s1, s2):
             union(a, b)
-        return "".join(minn[find(c)] for c in baseStr)
+        return "".join(parent[find(c)] for c in baseStr)
