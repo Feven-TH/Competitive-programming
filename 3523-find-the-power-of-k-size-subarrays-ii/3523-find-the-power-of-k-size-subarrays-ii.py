58399354ae24778708e95e8fdb086f529d7d9cc2
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-06-17 13:28:57 +0300

    Time: 90 ms (99.22%), Space: 34.1 MB (99.1%) - LeetHub

--- 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.py
+++ 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         res = [-1] * (len(nums) - k + 1)
         left = 0
-        if len(nums) == 1 or k==1:
+        if k==1:
             return nums
         for i in range(1, len(nums)):
             if nums[i] - nums[i - 1] != 1:
