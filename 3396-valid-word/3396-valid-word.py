80b393bb762e117c526c69fa8b66a3e02d23ce68
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-08-09 20:57:23 +0300

    Time: 0 ms (100%), Space: 17.7 MB (85.32%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        has_vowel = False
+        has_consonant = False
+
+        for ch in word:
+            if not ch.isalnum():
+                return False
+
+            if ch.isalpha():
+                lower = ch.lower()
+                if lower in "aeiou":
+                    has_vowel = True
+                else:
+                    has_consonant = True
+
+        return has_vowel and has_consonant
