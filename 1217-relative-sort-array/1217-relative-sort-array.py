4947b676716ad2b28c58e0066da6a097b1ce0c5b
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-08-18 20:14:28 +0300

    Time: 0 ms (100%), Space: 17.7 MB (90.83%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = Counter(arr1)
+        res = []
+
+        for num in arr2:
+            res.extend([num] *count[num])
+            del count[num] 
+
+        for num in sorted(count.keys()):
+            res.extend([num] *count[num])
+
+        return res
