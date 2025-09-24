72a63661850829aa6b1928df7e6e73ca25400610
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-09-24 15:20:15 +0300

    Time: 2 ms (2.91%), Space: 17.5 MB (95.62%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = defaultdict(int)
+        def dp(x):
+            if x == 0:
+                return 0
+            if x == 1 or x == 2:
+                return 1 
+            if x not in memo:
+                memo[x] = dp(x-1) + dp(x-2) + dp(x-3)
+            return memo[x]
+        return dp(n)
