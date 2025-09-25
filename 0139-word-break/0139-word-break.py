e5bc12deca897151cf4b3546ce4f41d1257a8fb3
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-09-25 19:19:44 +0300

    Time: 0 ms (100%), Space: 18.1 MB (25.03%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = defaultdict(bool)
+        def dp(i):
+            if i >= len(s):
+                return True
+            if i not in memo:
+                flag = False
+                for word in wordDict:
+                    if s.startswith(word,i):
+                        if dp(i + len(word)):
+                            flag = True
+                            return True
+                memo[i] = flag
+                return memo[i]
+        return dp(0)
