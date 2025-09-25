c3a575a36058b3a0090aa4325e9074e6fc70d880
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-09-25 14:43:32 +0300

    Time: 0 ms (100%), Space: 17.8 MB (33.93%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = defaultdict(int)
+        def dp(i):
+            if i == 0:
+                return nums[i]
+            if i == 1:
+                return max(nums[i], nums[i-1])
+            if i not in memo:
+                memo[i] = max(dp(i-2) + nums[i], dp(i-1))
+            return memo[i]
+        return dp(len(nums)-1)
+
+     
