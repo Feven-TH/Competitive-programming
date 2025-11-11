d966085abbcf668facb6489aeee20fecd41b341e
Feven Tewelde <168232686+Feven-TH@users.noreply.github.com>
2025-11-11 19:24:48 +0300

    Time: 7 ms (25.16%), Space: 17.8 MB (70.81%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,33 +1,51 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def valid(row,col):
-            return (0 <= row < len(grid)) and (0 <= col < len(grid[0])) and grid[row][col] == 1  
-
-        directions = [(0,1) , (1,0), (-1,0), (0,-1)]
+        row , col = len(grid) , len(grid[0])
+        
+        
+        directions = [(0 , 1) , (0, -1) , (1 , 0) , (-1 , 0)]
+        
+        def inbound(r , c):
+            return 0 <= r < row and 0 <= c < col
+        
+        fresh = 0
         queue = deque()
-        time , fresh = 0 , 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == 1:
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 2:
+                    queue.append((r , c))
+                if grid[r][c] == 1:
                     fresh += 1
-                if grid[row][col] == 2:
-                    queue.append((row,col))
                     
-        while queue and fresh > 0:
-            for _ in range(len(queue)):
-                i , j = queue.popleft()
+        if fresh == 0:
+            return 0
+        
+        time = 0
+        while queue:
+            
+            for i in range(len(queue)):
+                r , c = queue.popleft()
                 for dr , dc in directions:
-                    R = i + dr
-                    C = j + dc
-                    if valid(R,C):
-                        grid[R][C] = 2
+                    nr = r + dr
+                    nc = c + dc      
+                    if inbound(nr , nc) and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
                         fresh -= 1
-                        queue.append((R,C))
+                        queue.append((nr , nc))
             time += 1
+            
 
-        if fresh > 0:
-            return -1
-        else:
-            return time
+        if fresh:
+            return -1   
+        return time - 1
+                        
+                        
+        
 
-                
+        
+        
+        
+        
+        
+        
+        
